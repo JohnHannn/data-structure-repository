fd8175898cf0b521fbbc47b48f3ad0d879ccdee6
John Han <john.steve.han@gmail.com>
2023-06-08 14:21:58 +0800

    Update create-single-linked-list.c

--- create-single-linked-list.c
+++ create-single-linked-list.c
@@ -3,5 +3,28 @@
 #define MALLOC_EORR "malloc function failed!"
 struct node{
   int data;
-  struct node *head = (struct node *)malloc(size_of(struct node)); // alloct a node memory for one node
+  struct node *link; // self reference link
+};
+
+int main(){
+  struct node *head = NULL
+  head = (struct node *)malloc(size_of(struct node)); // alloct a node memory for one node
+  if (head == NULL){
+    printf("%s", MALLOC_EORR);
+    return 0;
+  }
+  head -> data = 45;
+  head -> link = NULL;
+  
+  struc node *current = NULL;
+  current = (struct node *)malloc(size_of(struct node)); // alloct a node memory for one node
+  if (current == NULL){
+    printf("%s", MALLOC_EORR);
+    return 0;
+  }
+  head->link = current; // first node point to the second node;
+  current->data = 3;
+  current->link = NULL; // end of a linked list
+  
+  return 0;
 }
